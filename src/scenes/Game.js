b7d59e90f6fd1f91f6a4337c426308e42536cb56
kamil.palka <cabeq1236@gmail.com>
2023-02-13 22:08:12 +0100

    add platforms to scene

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -12,6 +12,22 @@ export default class Game extends Phaser.Scene {
   create() {
     this.add.image(240, 320, "background");
     //this.add.image(240, 320, "platform").setScale(0.5);
-    this.physics.add.image(240, 320, "platform").setScale(0.5);
+    //this.physics.add.image(240, 320, "platform").setScale(0.5);
+
+    // create the group
+    const platforms = this.physics.add.staticGroup();
+
+    for (let i = 0; i < 5; ++i) {
+      const x = Phaser.Math.Between(80, 400);
+      const y = 150 * i;
+
+      /** @type {Phaser.Physics.Arcade.Sprite} */
+      const platform = platforms.create(x, y, "platform");
+      platform.scale = 0.5;
+
+      /** @type {Phaser.Physics.Arcade.StaticBody} */
+      const body = platform.body;
+      body.updateFromGameObject();
+    }
   }
 }
